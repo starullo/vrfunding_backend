39b7d90114dc93b0396a04b424f934c379e46fd5
Sam Tarullo <sam@Sams-Air.attlocal.net>
2020-11-17 17:02:24 -0600

    added funds thing

--- api/server.js
+++ api/server.js
@@ -3,7 +3,9 @@ const app = express();
 const userRouter = require('../users/usersRouter');
 const authRouter = require('../auth/authRouter');
 const eventsRouter = require('../events/eventsRouter');
+const db = require('../data/config');
 const cors = require('cors');
+const {secureLogin} = require('../auth/authMiddleware');
 
 app.use(express.json());
 app.use(cors());
@@ -13,6 +15,11 @@ app.use('/api/projects', eventsRouter)
 
 app.get('/api/test', (req, res, next)=>{
     res.json({message: 'affirmative'})
+})
+
+app.get('/api/funds', secureLogin, async (req, res, next)=>{
+    const funds = await db('donations');
+    res.json(funds)
 })
 
 app.use((err, req, res, next)=>{
